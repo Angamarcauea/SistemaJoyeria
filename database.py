0087d706e6d3d319a35963a0b6af741cbabbf9fc
Angamarcauea <jm.angamarcaa@uea.edu.ec>
2026-03-31 17:11:26 -0500

    Semana 15

--- database.py
+++ database.py
@@ -2,7 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joyeria.db'
+    # Formato: mysql+pymysql://usuario:password@localhost/nombre_db
+    # Cambia 'root' y 'tu_password' por tus credenciales de MySQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:adm123@localhost/joyerialuxury'
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
